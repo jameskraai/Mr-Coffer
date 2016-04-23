a4fde9b0a48dc94042d856635e36cdb9d0a49340
James Kraai <james@jameskraai.com>
2016-04-23 18:58:19 -0500

    MRCOF-3 Add schema to Accounts migration

--- database/migrations/2016_04_23_233517_create_accounts_table.php
+++ database/migrations/2016_04_23_233517_create_accounts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
@@ -12,7 +13,12 @@ class CreateAccountsTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('accounts', function(Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('number');
+            $table->string('type');
+        });
     }
 
     /**
