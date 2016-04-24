e2c3a88d069946196e0e6c84a945ffebf63c474b
James Kraai <james@jameskraai.com>
2016-04-23 23:21:30 -0500

    MRCOF-5 Add foreign key constraints and a rollback

--- database/migrations/2016_04_24_034203_add_foreign_keys.php
+++ database/migrations/2016_04_24_034203_add_foreign_keys.php
@@ -16,6 +16,12 @@ class AddForeignKeys extends Migration
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('type_id')->references('id')->on('accountTypes');
         });
+
+        Schema::table('transactions', function(Blueprint $table) {
+            $table->foreign('account_id')->references('id')->on('accounts');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('payee_id')->references('id')->on('payees');
+        });
     }
 
     /**
@@ -28,6 +34,12 @@ class AddForeignKeys extends Migration
         Schema::table('accounts', function(Blueprint $table) {
             $table->dropForeign(['user_id']);
             $table->dropForeign(['type_id']);
+        });
+
+        Schema::table('transactions', function(Blueprint $table) {
+            $table->dropForeign(['account_id']);
+            $table->dropForeign(['category_id']);
+            $table->dropForeign(['payee_id']);
         });
     }
 }
