08d61ba6630b2aba484d067527bfb41502980f1f
James Kraai <james@jameskraai.com>
2016-04-24 20:08:07 -0500

    MRCOF-6 Fix issue where login form was posting to the wrong route.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 <!-- resources/views/auth/login.blade.php -->
 
-<form method="POST" action="/auth/login">
+<form method="POST" action="/login">
     {!! csrf_field() !!}
     <label for="email">Email</label>
     <input type="email" name="email" value="{{ old('email') }}" id="email">
