7b93b23c0cbf4854855f004990ed2fcee3d74b82
James Kraai <james@jameskraai.com>
2016-05-02 22:24:57 -0500

    MRCOF-26 Display the logged in the user name on the blade template.

--- resources/views/dashboard/main.blade.php
+++ resources/views/dashboard/main.blade.php
@@ -5,6 +5,8 @@
     </head>
     <body>
         <main>
+            <h1>Dashboard</h1>
+            <h3>{{ $user->name }}</h3>
             <a href="{!! route('logout') !!}">Logout</a>
         </main>
     </body>
