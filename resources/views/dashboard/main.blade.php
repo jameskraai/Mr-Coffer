1f29cbffff0c115c07d2536d2986b6079f37c9fb
James Kraai <james@jameskraai.com>
2016-05-03 23:51:13 -0500

    MRCOF-36 Display a list of all of the Account Numbers

--- resources/views/dashboard/main.blade.php
+++ resources/views/dashboard/main.blade.php
@@ -8,6 +8,15 @@
             <h1>Dashboard</h1>
             <h3>{{ $user->name }}</h3>
             <a href="{!! route('logout') !!}">Logout</a>
+
+            <section id="accounts">
+                <h3>Accounts</h3>
+                <ul>
+                    @foreach($user->accounts as $account)
+                        <li>{{ $account->number }}</li>
+                    @endforeach
+                </ul>
+            </section>
         </main>
     </body>
 </html>
