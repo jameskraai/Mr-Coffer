6a3f9f0f0d304044137ec98cfff93b8e9b09662d
James Kraai <james@jameskraai.com>
2016-05-06 22:55:22 -0500

    MRCOF-35 Add master blade template which yields the main section to each route.

--- resources/views/dashboard/main.blade.php
+++ resources/views/dashboard/main.blade.php
@@ -1,32 +1,24 @@
-<!DOCTYPE HTML>
-<html>
-    <head>
-
-    </head>
-    <body>
-        <main>
-            <h1>Dashboard</h1>
-            <h3>{{ $user->name }}</h3>
-            <a href="{!! route('logout') !!}">Logout</a>
-            <table>
-                <thead>
-                    <tr><th>Accounts</th></tr>
-                    <tr>
-                        <th>Name</th>
-                        <th>Number</th>
-                        <th>Edit</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($user->accounts as $account)
-                        <tr>
-                            <td><a href="{!! route('account.show', [$account->id]) !!}">{{ $account->name }}</a></td>
-                            <td>{{ $account->number }}</td>
-                            <td><a href="{!! route('account.edit', [$account->id]) !!}">Edit</a></td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </main>
-    </body>
-</html>
+@extends('master')
+@section('content')
+    <h1>Dashboard</h1>
+    <h3>{{ $user->name }}</h3>
+    <table>
+        <thead>
+            <tr><th>Accounts</th></tr>
+            <tr>
+                <th>Name</th>
+                <th>Number</th>
+                <th>Edit</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($user->accounts as $account)
+                <tr>
+                    <td><a href="{!! route('account.show', [$account->id]) !!}">{{ $account->name }}</a></td>
+                    <td>{{ $account->number }}</td>
+                    <td><a href="{!! route('account.edit', [$account->id]) !!}">Edit</a></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
