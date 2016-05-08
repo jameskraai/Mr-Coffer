2cf205abf4bfe02d41f93e13a12d554519dc7b6d
James Kraai <james@jameskraai.com>
2016-05-08 16:07:26 -0500

    MRCOF-35 account.show.blade Display all of the Transaction details

--- resources/views/account/show.blade.php
+++ resources/views/account/show.blade.php
@@ -1,2 +1,38 @@
 @extends('master')
 <h1>{{  $account->name }}</h1>
+<table>
+    <thead>
+        <tr><th>Transactions</th></tr>
+        <tr>
+            <th>Payee</th>
+            <th>Memo</th>
+            <th>Amount</th>
+            <th>Category</th>
+            <th>Status</th>
+            <th>Updated</th>
+            <th>Created</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($account->transactions as $transaction)
+            <tr>
+                {{-- Payee --}}
+                <td>{{ $transaction->payee->name }}</td>
+
+                {{-- Memo --}}
+                <td>{{ $transaction->memo }}</td>
+                {{-- Amount --}}
+                <td>{{ $transaction->amount }}</td>
+                {{-- Category --}}
+                <td>{{ $transaction->category->name }}</td>
+                {{-- Status --}}
+                <td>{{ $transaction->status->name }}</td>
+                {{-- Updated --}}
+                <td>{{ $transaction->updated_at }}</td>
+
+                {{-- Created --}}
+                <td>{{ $transaction->created_at }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
