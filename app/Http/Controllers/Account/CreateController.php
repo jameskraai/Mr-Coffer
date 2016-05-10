0107c0e399699d144ba4a2438c96cb0d4887588a
James Kraai <james@jameskraai.com>
2016-05-09 22:40:21 -0500

    MRCOF-30 Add Account.CreateController

---
+++ app/Http/Controllers/Account/CreateController.php
@@ -0,0 +1,14 @@
+<?php namespace MrCoffer\Http\Controllers\Account;
+
+use MrCoffer\Http\Controllers\Controller;
+
+/**
+ * Class CreateController
+ * @package MrCoffer\Http\Controllers\Account
+ */
+class CreateController extends Controller
+{
+    public function create()
+    {
+    }
+}
