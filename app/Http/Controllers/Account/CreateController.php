0b53cdf2f2eb36d1137ab055ccaa79adf1fb1de0
James Kraai <james@jameskraai.com>
2016-05-11 22:51:59 -0500

    MRCOF-30 Pass Account Types and Banks to the create view.

--- app/Http/Controllers/Account/CreateController.php
+++ app/Http/Controllers/Account/CreateController.php
@@ -1,7 +1,10 @@
 <?php namespace MrCoffer\Http\Controllers\Account;
 
+use MrCoffer\Bank;
+use Illuminate\Support\Collection;
 use Illuminate\View\Factory as View;
 use MrCoffer\Http\Controllers\Controller;
+use MrCoffer\Account\Type as AccountType;
 
 /**
  * Class CreateController
@@ -9,8 +12,45 @@ use MrCoffer\Http\Controllers\Controller;
  */
 class CreateController extends Controller
 {
+    /**
+     * Bank model instance.
+     *
+     * @var Bank
+     */
+    protected $bank;
+
+    /**
+     * Account Type model instance.
+     *
+     * @var AccountType
+     */
+    protected $accountType;
+
+    /**
+     * CreateController constructor.
+     * @param Bank $bank
+     * @param AccountType $accountType
+     */
+    public function __construct(Bank $bank, AccountType $accountType)
+    {
+        $this->bank = $bank;
+        $this->accountType = $accountType;
+    }
+
+
+    /**
+     * Create a new Account view.
+     *
+     * @param View $view
+     * @return \Illuminate\Contracts\View\View
+     */
     public function create(View $view)
     {
-        return $view->make('account.create', []);
+        $data = [
+            'accountTypes' => $this->accountType->all(),
+            'banks' => $this->bank->all(),
+        ];
+
+        return $view->make('account.create', $data);
     }
 }
