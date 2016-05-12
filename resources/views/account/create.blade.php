ac328dc1a92b018a17db822d5e32f7af268cb82e
James Kraai <james@jameskraai.com>
2016-05-11 22:29:39 -0500

    MRCOF-30 account.create view Add csrf hidden token field

--- resources/views/account/create.blade.php
+++ resources/views/account/create.blade.php
@@ -1,2 +1,8 @@
 @extends('master')
 <h1>Create a new Account</h1>
+<form action="{!! route('account.store') !!}" method="post">
+    {{ csrf_field()  }}
+    <label for="number">Number</label>
+    <input type="text" name="number" id="number">
+    <input type="submit" value="Save">
+</form>
