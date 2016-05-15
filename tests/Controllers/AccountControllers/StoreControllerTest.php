19e042654050aa388c6f492595eab594d9f2e117
James Kraai <james@jameskraai.com>
2016-05-14 21:52:14 -0500

    MRCOF-43 Account\StoreCtrl test Move db factories back to the method test.

--- tests/Controllers/AccountControllers/StoreControllerTest.php
+++ tests/Controllers/AccountControllers/StoreControllerTest.php
@@ -1,10 +1,12 @@
 <?php namespace MrCoffer\Tests\AccountController;
 
+use Mockery;
 use MrCoffer\User;
 use MrCoffer\Bank;
 use MrCoffer\Tests\TestCase;
 use MrCoffer\Account\Type as AccountType;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use MrCoffer\Http\Controllers\Account\StoreController;
 
 /**
  * Class StoreControllerTest
@@ -17,36 +19,6 @@ class StoreControllerTest extends TestCase
     use DatabaseMigrations;
 
     /**
-     * Factory created Account Type Eloquent Model.
-     *
-     * @var AccountType
-     */
-    public $accountType;
-
-    /**
-     * Factory created Bank Model.
-     *
-     * @var Bank
-     */
-    public $bank;
-
-    /**
-     * Pre-test setting up.
-     *
-     * @return void
-     */
-    public function setUp()
-    {
-        parent::setUp();
-
-        // Generate the temporary Eloquent Models that are required
-        // to test this Http Controller.
-        $this->accountType = factory(AccountType::class)->create();
-        $this->bank = factory(Bank::class)->create();
-        $this->user = factory(User::class)->create();
-    }
-
-    /**
      * Test that the the new Account.create view is ble to be used
      * to successfully create a new Account instance.
      *
@@ -54,15 +26,19 @@ class StoreControllerTest extends TestCase
      */
     public function testAccountCanBeMade()
     {
+        $accountType = factory(AccountType::class)->create();
+        $bank = factory(Bank::class)->create();
+        $user = factory(User::class)->create();
+
         // The following portion tests that an authenticated User is able to visit the 'account/create' view
         // then fill out the form with valid values and then save - of which then a new Account is saved
         // to the database and the User is redirect back to the dashboard without error.
-        $this->actingAs($this->user);
+        $this->actingAs($user);
         $this->visit('account/create');
         $this->type('New Account', 'name');
         $this->type(2143, 'number');
-        $this->select($this->accountType->getAttribute('id'), 'account-type');
-        $this->select($this->bank->getAttribute('id'), 'bank');
+        $this->select($accountType->getAttribute('id'), 'account-type');
+        $this->select($bank->getAttribute('id'), 'bank');
         $this->press('save');
         $this->seePageIs('/dashboard');
 
