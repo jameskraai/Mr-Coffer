c548ae32e4845e9193aedf47cf72637c379d914d
James Kraai <james@jameskraai.com>
2016-07-27 20:14:43 -0500

    MRCOF-32 Update routes file to not be enclosed within a closure

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,52 +6,50 @@ use Illuminate\Routing\Router;
  * Declare all Http Routes within this callback function. This function will be invoked with
  * a Router instance injected into it at the RouteServiceProvider class.
  *
- * @param Router $route
+ * @var Router $route
  *
  * @see \MrCoffer\Providers\RouteServiceProvider
  * @return void
  */
-return function (Router $route) {
 
-    /**
-     * Dashboard
-     * When the User is logged in they will be routed to the dashboard. This is where
-     * they can manage all of their Transactions, Accounts, etc.
-     */
-    $route->get('/', 'DashboardController@index');
-    $route->get('/dashboard', ['as' => 'dashboard', 'uses' => 'DashboardController@index']);
+/**
+ * Dashboard
+ * When the User is logged in they will be routed to the dashboard. This is where
+ * they can manage all of their Transactions, Accounts, etc.
+ */
+$route->get('/', 'DashboardController@index');
+$route->get('/dashboard', ['as' => 'dashboard', 'uses' => 'DashboardController@index']);
 
-    /**
-     * Authentication
-     * The User should be able to visit a page with a login form, submit a login request
-     * to said form and also visit a route to log out of the app.
-     */
-    $route->get('/login', ['as' => 'login', 'uses' => 'Auth\AuthController@getLogin']);
-    $route->post('/login', 'Auth\AuthController@postLogin');
-    $route->get('/logout', ['as' => 'logout', 'uses', 'Auth\AuthController@getLogout']);
+/**
+ * Authentication
+ * The User should be able to visit a page with a login form, submit a login request
+ * to said form and also visit a route to log out of the app.
+ */
+$route->get('/login', ['as' => 'login', 'uses' => 'Auth\AuthController@getLogin']);
+$route->post('/login', 'Auth\AuthController@postLogin');
+$route->get('/logout', ['as' => 'logout', 'uses', 'Auth\AuthController@getLogout']);
 
-    /**
-     * Provide a route to a registration form as well as
-     * handling for new User registration.
-     */
-    $route->get('/register', ['as' => 'register', 'uses' => 'Auth\AuthController@getRegister']);
-    $route->post('/register', 'Auth\AuthController@Register');
+/**
+ * Provide a route to a registration form as well as
+ * handling for new User registration.
+ */
+$route->get('/register', ['as' => 'register', 'uses' => 'Auth\AuthController@getRegister']);
+$route->post('/register', 'Auth\AuthController@Register');
 
-    /**
-     * Account
-     */
-    // The Account Create page.
-    $route->get('/account/create', ['as' => 'account.create', 'uses' => 'Account\CreateController@create']);
+/**
+ * Account
+ */
+// The Account Create page.
+$route->get('/account/create', ['as' => 'account.create', 'uses' => 'Account\CreateController@create']);
 
-    // Submit a request to make a new Account.
-    $route->post('/account/create', ['as' => 'account.store', 'uses' => 'Account\StoreController@store']);
+// Submit a request to make a new Account.
+$route->post('/account/create', ['as' => 'account.store', 'uses' => 'Account\StoreController@store']);
 
-    // Show a page to Edit a single Account.
-    $route->get('/account/{id}/edit', ['as' => 'account.edit', 'uses' => 'Account\EditController@edit']);
+// Show a page to Edit a single Account.
+$route->get('/account/{id}/edit', ['as' => 'account.edit', 'uses' => 'Account\EditController@edit']);
 
-    // Submit an update request to a single Account.
-    $route->patch('/account/{id}/edit', ['as' => 'account.edit', 'uses' => 'Account\PatchController@patch']);
+// Submit an update request to a single Account.
+$route->patch('/account/{id}/edit', ['as' => 'account.edit', 'uses' => 'Account\PatchController@patch']);
 
-    // Submit a request to view a single Account.
-    $route->get('/account/{id}', ['as' => 'account.show', 'uses' => 'Account\ShowController@show']);
-};
+// Submit a request to view a single Account.
+$route->get('/account/{id}', ['as' => 'account.show', 'uses' => 'Account\ShowController@show']);
