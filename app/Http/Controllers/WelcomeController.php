92e20c4cda0dfb012e9d6f59a06cc44335a4050e
James Kraai <james.kraai@wholefoods.com>
2016-09-14 14:01:56 -0500

    MRCOF-95 Get the authenticated User via the API Auth Guard class. Return a success message when an incoming request contains a valid token.

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,14 +2,31 @@
 
 namespace MrCoffer\Http\Controllers;
 
+use MrCoffer\User;
+use Illuminate\Auth\AuthManager;
+
 /**
- * Class Dashboard
+ * Class WelcomeController
+ * Default route which indicates whether the User is authenticated or not.
  */
 class WelcomeController extends Controller
 {
-    public function __construct()
+    /**
+     * Auth Manager, this is used to retrieve the
+     * User authorized with the current Token.
+     *
+     * @var AuthManager
+     */
+    protected $authManager;
+
+    /**
+     * WelcomeController constructor.
+     * @param AuthManager $authManager
+     */
+    public function __construct(AuthManager $authManager)
     {
-        $this->middleware('wantJson');
+        $this->middleware(['wantJson', 'auth:api']);
+        $this->authManager = $authManager;
     }
 
     /**
@@ -19,8 +36,16 @@ class WelcomeController extends Controller
      */
     public function index()
     {
-        $json = json_encode(['You have arrived.']);
+        /**
+         * The User that is currently authenticated
+         * via the bearer token.
+         *
+         * @var User $user
+         */
+        $user = $this->authManager->guard('api')->user();
 
-        return $json;
+        $message = "You have arrived {$user->getAttribute('name')}.";
+
+        return json_encode([$message]);
     }
 }
