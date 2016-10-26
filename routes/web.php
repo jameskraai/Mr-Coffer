eeeab958355b84151a0dbfd0a4a8fcd1fadbd2c6
James Kraai <james@jameskraai.com>
2016-10-25 21:52:20 -0500

    GH-24 Restore the ability for a User to login.

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 |
 */
 Auth::routes();
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->middleware(['auth']);
+Route::get('/account/create', 'Account\CreateController@create')->name('account.create');
+Route::get('/account/{id}', 'Account\ShowController@show')->name('account.show');
+Route::get('/account/{id}/edit', 'Account\EditController@edit')->name('account.edit');  
